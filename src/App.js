1f2f432e61386504ba20ea85cb791fbbd22c76e2
frankster1ful <47283968+franskter1ful@users.noreply.github.com>
2023-04-10 17:18:18 -0500

    updated App.js content
    testing pipeline editing from GitHub web interface

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>New Closed Captions Project</h1>
+        <h1>Frank GMZ TESTING PIPELINE</h1>
       </header>
     </div>
   );
